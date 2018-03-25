4dea2c0dd8f2c142663d71368f27aa401070e315
D0miq <dpoch@email.cz>
2018-03-25 22:53:15 +0200

    Animace

--- Viewer/public_html/viewer.js
+++ Viewer/public_html/viewer.js
@@ -50,19 +50,25 @@ class FileReader3BA {
         this.readFaces = function () {
             var facesLength = new Int32Array(getContent(4))[0];
             console.log("Faces length", facesLength);
-            return new Float32Array(getContent(facesLength * 4 * 9));
+            var allFaces = new Int32Array(getContent(facesLength * 4 * 9));
+            var j = 0;
+            this.faces = [];
+            for(var i = 0; i < allFaces.length; i += 3) {
+                this.faces[j] = allFaces[i] - 1;
+                j++;
+            }
         };
         
         this.readTextures = function () {
             var texturesLength = new Int32Array(getContent(4))[0];
             console.log("Textures length", texturesLength);
-            return texturesLength == 0 ? null : new Float32Array(getContent(texturesLength * 4));
+            this.textures = texturesLength == 0 ? null : new Float32Array(getContent(texturesLength * 4));
         };
         
         this.readNormals = function () {
             var normalsLength = new Int32Array(getContent(4))[0];
             console.log("Normals length", normalsLength);
-            return normalsLength == 0 ? null : new Float32Array(getContent(normalsLength * 4));
+            this.normals = normalsLength == 0 ? null : new Float32Array(getContent(normalsLength * 4));
         };
     }
     
@@ -72,9 +78,10 @@ class FileReader3BA {
         console.log(eigenVectors);
         var controlTrajectories = this.readControlTrajectories();
         console.log(controlTrajectories);
-        this.faces = this.readFaces();
-        this.textures = this.readTextures();
-        this.normals = this.readNormals();
+        this.readFaces();
+        this.readTextures();
+        this.readNormals();
+        
         var s = math.multiply(eigenVectors, controlTrajectories);
         var b = s.map(function (value, index, matrix) {
             return value + averageTrajectory[index[0]];
@@ -93,6 +100,10 @@ class FileReader3BA {
             this.vertices[i / 3] = frame;
         }
    }
+}
+
+class TextureReader {
+    
 }
 
 //---------------------------------- Shaders ----------------------------------
@@ -236,7 +247,7 @@ class CameraMovement {
 
 //---------------------------------- Webgl ----------------------------------
 class Webgl {
-    constructor(canvas, camera, vertices, faces, textures = null, normals = null) {
+    constructor(canvas, camera, vertices, faces, textures, normals) {
         console.log("Canvas element:", canvas);
         this.camera = camera;
         this.vertices = vertices;
@@ -261,36 +272,48 @@ class Webgl {
     }
     
     startAnimation() {
-        var fragmentShader = new FragmentShader();
-        var vertexShader = new VertexShader();
+        // --------------------- Initialize shaders ---------------------
+        {
+            var fragmentShader = new FragmentShader();
+            var vertexShader = new VertexShader();
 
-        //create shader program
-        var shaderProgram = gl.createProgram();
-        gl.attachShader(shaderProgram, fragmentShader.build());
-        gl.attachShader(shaderProgram, vertexShader.build());
-        gl.linkProgram(shaderProgram);
+            //create shader program
+            var shaderProgram = gl.createProgram();
+            gl.attachShader(shaderProgram, fragmentShader.build());
+            gl.attachShader(shaderProgram, vertexShader.build());
+            gl.linkProgram(shaderProgram);
 
-        //if creating shader program failed
-        if(!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)){
-          console.log("Unable to initialize the shader program:", gl.getProgramInfoLog(shaderProgram));
+            //if creating shader program failed
+            if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+                console.log("Unable to initialize the shader program:", gl.getProgramInfoLog(shaderProgram));
+            }
+
+            var vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+            var projectionMatrixLocation = gl.getUniformLocation(shaderProgram, 'uProjectionMatrix');
+            var modelViewMatrixLocation = gl.getUniformLocation(shaderProgram, 'uModelViewMatrix');
         }
         
-        var vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
-        var projectionMatrixLocation = gl.getUniformLocation(shaderProgram, 'uProjectionMatrix');
-        var modelViewMatrixLocation = gl.getUniformLocation(shaderProgram, 'uModelViewMatrix');
-        
-        console.log("Initialize buffers"); 
-        
-        var positionBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vertices), gl.STATIC_DRAW);
+        // --------------------- Initialize buffers ---------------------
+        {
+            console.log("Initialize buffers");
 
+            var positionBuffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+            
+            var indicesBuffer = gl.createBuffer();
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indicesBuffer);
+            gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.faces), gl.STATIC_DRAW);
+        }
+        
         var camera = this.camera;
+        var faces = this.faces;
         var vertices = this.vertices;
+        var animationIndex = 0;
+        var then = 0;
         
         requestAnimationFrame(drawScene);
         
-        function drawScene() {
+        function drawScene(now) {
             console.log("Drawing a scene");
             //set background
             gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -302,33 +325,48 @@ class Webgl {
             gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
             gl.useProgram(shaderProgram);
-
-            gl.enableVertexAttribArray(vertexPositionAttribute);
-            gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-            gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
-
-            var projectionMatrix = mat4.create();
-            mat4.perspective(projectionMatrix, Math.PI / 4, gl.canvas.clientWidth / gl.canvas.clientHeight, 0.1, 100.0);
             
-            var modelViewMatrix = mat4.create();
-            mat4.translate(modelViewMatrix, modelViewMatrix, camera.getTranslation());
-            mat4.rotateX(modelViewMatrix, modelViewMatrix, camera.getRotation()[0]);
-            mat4.rotateY(modelViewMatrix, modelViewMatrix, camera.getRotation()[1]);
-            mat4.rotateZ(modelViewMatrix, modelViewMatrix, camera.getRotation()[2]);
-            mat4.scale(modelViewMatrix, modelViewMatrix, camera.getScaling());
-           
-            gl.uniformMatrix4fv(projectionMatrixLocation, false, projectionMatrix);
-            gl.uniformMatrix4fv(modelViewMatrixLocation, false, modelViewMatrix);
+            // --------------------- Buffers ---------------------
+            {
+                gl.enableVertexAttribArray(vertexPositionAttribute);
+                gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+                gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices[animationIndex]), gl.DYNAMIC_DRAW);
+                gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+                
+                // Tell WebGL which indices to use to index the vertices
+                gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indicesBuffer);
+            }
+            
+            // --------------------- Transformations ---------------------
+            {
+                var projectionMatrix = mat4.create();
 
-            gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertices.length / 3);
+                mat4.perspective(projectionMatrix, Math.PI / 4, gl.canvas.clientWidth / gl.canvas.clientHeight, 0.1, 100.0);
 
+                var modelViewMatrix = mat4.create();
+                mat4.translate(modelViewMatrix, modelViewMatrix, camera.getTranslation());
+                mat4.rotateX(modelViewMatrix, modelViewMatrix, camera.getRotation()[0]);
+                mat4.rotateY(modelViewMatrix, modelViewMatrix, camera.getRotation()[1]);
+                mat4.rotateZ(modelViewMatrix, modelViewMatrix, camera.getRotation()[2]);
+                mat4.scale(modelViewMatrix, modelViewMatrix, camera.getScaling());
+
+                gl.uniformMatrix4fv(projectionMatrixLocation, false, projectionMatrix);
+                gl.uniformMatrix4fv(modelViewMatrixLocation, false, modelViewMatrix);
+            }
+            
+            gl.drawElements(gl.TRIANGLES, faces.length, gl.UNSIGNED_SHORT, 0);
+            
+            now *= 0.001;
+            var deltaTime = now - then;
+            then = now;
+            animationIndex = Math.floor(animationIndex + 60 * deltaTime) % vertices.length;
             requestAnimationFrame(drawScene);
         }
     }
 }
 
 //---------------------------------- Main function ----------------------------------
-function main(canvas, path){
+function main(canvas, path, texture){
     console.log("Starting webgl viewer");
     
     console.log("Downloading file", path, "from the server");
@@ -342,17 +380,10 @@ function main(canvas, path){
         if (file) {
             var fileReader = new FileReader3BA(file);
             fileReader.readFile();
-            
-            /*var positions = [
-                1.0, 1.0, 
-                -1.0, 1.0,  
-                1.0, -1.0, 
-                -1.0, -1.0,
-            ];*/
-            
+
             canvas = document.getElementById(canvas);
             var camera = new CameraMovement(canvas);
-            var webgl = new Webgl(canvas, camera, fileReader.vertices[0]);
+            var webgl = new Webgl(canvas, camera, fileReader.vertices, fileReader.faces);
             
             if (!gl) {
                 return;
