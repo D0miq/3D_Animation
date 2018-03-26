0a44ae6ecaa0059bfdeebc62b4f1ee2b71e6c04c
D0miq <dpoch@email.cz>
2018-03-26 22:05:12 +0200

    Čtení obrázku s texturou

--- Viewer/public_html/viewer.js
+++ Viewer/public_html/viewer.js
@@ -103,7 +103,37 @@ class FileReader3BA {
 }
 
 class TextureReader {
-    
+    constructor(url) {
+        this.texture = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, this.texture);
+
+        const pixel = new Uint8Array([190, 190, 190, 255]);  // opaque blue
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA,
+                1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
+                pixel);
+
+        const image = new Image();
+        image.onload = function () {
+            gl.bindTexture(gl.TEXTURE_2D, this.texture);
+            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA,
+                    gl.RGBA, gl.UNSIGNED_BYTE, image);
+
+            // WebGL1 has different requirements for power of 2 images
+            // vs non power of 2 images so check if the image is a
+            // power of 2 in both dimensions.
+            if ((image.width & (image.width - 1)) == 0 && (image.height & (image.height - 1)) == 0) {
+                // Yes, it's a power of 2. Generate mips.
+                gl.generateMipmap(gl.TEXTURE_2D);
+            } else {
+                // No, it's not a power of 2. Turn of mips and set
+                // wrapping to clamp to edge
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+            }
+        };
+        image.src = url;
+    }
 }
 
 //---------------------------------- Shaders ----------------------------------
@@ -207,15 +237,15 @@ class CameraMovement {
         
         this.getTranslation = function() {
             return translation;
-        }
+        };
         
         this.getRotation = function() {
             return rotation;
-        }
+        };
         
         this.getScaling = function() {
             return scaling;
-        }
+        };
         
         canvas.onmousedown = function(event) {
             clickX = event.clientX;
@@ -267,6 +297,7 @@ class Webgl {
         }
         
         function computeNormals(vertices, faces) {
+            var normals = [];
             
         }
     }
